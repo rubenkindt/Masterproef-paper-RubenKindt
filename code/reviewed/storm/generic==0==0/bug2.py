d611e85c86f908b7de58b3615f1def9809b2fea6
RubenKindt <rubenKindt@live.be>
2022-12-12 22:07:41 +0100

    A lot of spelling later

--- code/reviewed/storm/generic==0==0/bug2.py
+++ code/reviewed/storm/generic==0==0/bug2.py
@@ -1,4 +1,6 @@
 from cpmpy import *
+from cpmpy.transformations.flatten_model import flatten_constraint, flatten_model
+from cpmpy.transformations.get_variables import print_variables
 
 X = intvar(lb=0, ub=9, name='X')
 m = Model()
@@ -7,6 +9,11 @@ m += ~(~(X == 3))
 
 m.solve(solver="gurobi")
 print(m.status().exitstatus.name) # UNSATISFIABLE
+
+# mf = flatten_model(m)
+print_variables(m)
+print(m)
+
 m.solve(solver="ortools")
 print(m.status().exitstatus.name) # UNSATISFIABLE
 m.solve(solver="minizinc:chuffed")
