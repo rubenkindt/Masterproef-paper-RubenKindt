c13538382059bc366eb1fb750c9132f3dbf8bd53
Ruben Kindt <rubenkindt@live.be>
2023-01-31 14:50:22 +0100

    processing last minute feedback

--- Milestones/M10 defence/send_more_money.py
+++ Milestones/M10 defence/send_more_money.py
@@ -7,12 +7,11 @@ model = Model()
 model += sum([s,e,n,d] * np.array([1000, 100, 10, 1])) \
        + sum([m,o,r,e] * np.array([1000, 100, 10, 1])) \
       == sum([m,o,n,e,y] * np.array([10000, 1000, 100, 10, 1]))
-   
 model += s > 0
 model += m > 0   
 model += AllDifferent([s,e,n,d,m,o,r,y])
 
-model.solve()
+model.solve(solver="minizinc:chuffed")
 print("  S,E,N,D =   ", [x.value() for x in [s,e,n,d]])
 print("  M,O,R,E =   ", [x.value() for x in [m,o,r,e]])
 print("M,O,N,E,Y ="   , [x.value() for x in [m,o,n,e,y]])
